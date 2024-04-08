1ccfbba465d3d0936550c89e06471d6f31b49795
CHEGEBB <chegephil24@gmail.com>
2024-04-08 13:20:56 +0300

    scss

--- src/index.js
+++ src/index.js
@@ -7,11 +7,8 @@ import bgCafeImage from './images/bg-cafe.jpg';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Use a container with the background image */}
     <div className="container">
-      {/* Black background covering 70% of the viewport height */}
       <div className="black-background"></div>
-      {/* Image covering the remaining top portion */}
       <img src={bgCafeImage} alt="Cafe background" className="cafe-image" />
     </div>
     <App />
