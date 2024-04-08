c7d9e088ff056d3e82f86f9d63986343fc0d19cd
CHEGEBB <chegephil24@gmail.com>
2024-04-08 13:46:55 +0300

    js

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ function App() {
   <div className="products-row-one">
     <div className="product">
       <img src="images/product-1.jpg" alt="Product 1" />
-      <h2>Product 1</h2>
+      <h2>Popular</h2>
       <p>Product 1 Description</p>
     </div>
     <div className="product">
       <img src="images/product-2.jpg" alt="Product 2" />
-      <h2>Product 2</h2>
+      <h2>Popular</h2>
       <p>Product 2 Description</p>
     </div>
     <div className="product">
       <img src="images/product-3.jpg" alt="Product 3" />
-      <h2>Product 3</h2>
+      <h2>Popular</h2>
       <p>Product 3 Description</p>
     </div>
   </div>
