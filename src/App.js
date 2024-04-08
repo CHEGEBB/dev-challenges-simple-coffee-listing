59f50c1e113d5a3f918027c5f65188792672cf1e
CHEGEBB <chegephil24@gmail.com>
2024-04-08 12:25:39 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@
 function App() {
   return (
     <div className="App">
+<h1 className="heading">Our Collection</h1>
+<p>Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
+<button>All Products</button>
+<button>Available Now</button>
 
     </div>
   );
